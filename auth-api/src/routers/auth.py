6eb882094c4289fcba29843bc00ef64235561cf4
Edwin M <edwingmundo2@gmail.com>
2024-02-11 16:24:47 -0500

    add method to create jwt
    adds a method that creates a signed jwt for auth
    
    This is a WIP but I got to see a friend for dinner :D

--- auth-api/src/routers/auth.py
+++ auth-api/src/routers/auth.py
@@ -5,7 +5,9 @@ from sqlalchemy.orm import Session, declarative_base
 from sqlalchemy.sql import func
 from pathlib import Path
 from pydantic import BaseModel, EmailStr
-import bcrypt 
+import bcrypt
+import jwt
+import datetime
 
 router = APIRouter(
     prefix="/api/v1/auth",
@@ -14,22 +16,29 @@ logger = setup_logging()
 
 Base = declarative_base()
 
+
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
     email = Column(String, unique=True)
-    hashed_password = Column(String) 
+    hashed_password = Column(String)
     date_created = Column(DateTime(timezone=True), server_default=func.now())
     date_updated = Column(DateTime(timezone=True), onupdate=func.now())
 
     def __repr__(self):
-        return f"<User(name={self.name}, email={self.email}, date_created={self.date_created}, date_updated={self.date_updated})>"
+        return (
+            f"<User(name={self.name}, email={self.email},"
+            f" date_created={self.date_created},"
+            f" date_updated={self.date_updated})>"
+        )
+
 
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
 
 def hash_password(password: str) -> str:
     """
@@ -41,12 +50,13 @@ def hash_password(password: str) -> str:
     Returns:
     str: The hashed and salted password.
     """
-    password_bytes = password.encode('utf-8')
-    
+    password_bytes = password.encode("utf-8")
+
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password_bytes, salt)
-    
-    return hashed_password.decode('utf-8')
+
+    return hashed_password.decode("utf-8")
+
 
 def verify_password(submitted_password: str, stored_hash: str) -> bool:
     """
@@ -59,25 +69,62 @@ def verify_password(submitted_password: str, stored_hash: str) -> bool:
     Returns:
     bool: True if the password matches the hash, False otherwise.
     """
-    submitted_password_bytes = submitted_password.encode('utf-8')
-    stored_hash_bytes = stored_hash.encode('utf-8')
+    submitted_password_bytes = submitted_password.encode("utf-8")
+    stored_hash_bytes = stored_hash.encode("utf-8")
     return bcrypt.checkpw(submitted_password_bytes, stored_hash_bytes)
+
+
+def create_jwt_token(user_id: int, secret_key: str, expires_delta: int = 60) -> str:
+    """
+    Create a JWT token.
+
+    Args:
+    user_id (int): User identifier to include in the token.
+    secret_key (str): The secret key used to sign the token.
+    expires_delta (int): Token expiration time in minutes.
+
+    Returns:
+    str: Encoded JWT token.
+    """
+    expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=expires_delta)
+    iss = "ecorp"
+    sub = "ecorp"
+    aud = "device-id"
+    payload = {
+        "user_id": user_id,
+        "iss": iss,
+        "sub": sub,
+        "aud": aud,
+        "exp": expire,
+    }
+
+    token = jwt.encode(payload, secret_key, algorithm="HS256")
+
+    return token
+
 
 @router.get("/")
 def test_auth():
     return "Welcome to the auth api"
+
 
 @router.post("/login")
 async def login(user_login: UserLogin):
     db_path = Path("auth.db").absolute()
     engine = create_engine(rf"sqlite:///{db_path}")
     Base.metadata.create_all(engine)
-    logger.warning(f"Request tried to loggin to {user_login.email} with password {user_login.password}")
+    logger.warning((
+        f"Request tried to loggin to {user_login.email}"
+        f" with password {user_login.password}"
+    ))
     with Session(engine) as session:
         user_record = session.query(User).filter(User.email == user_login.email).first()
         if user_record:
-            if verify_password(user_login.password, str(user_record.hashed_password)):
+            is_password_verified = verify_password(
+                user_login.password, str(user_record.hashed_password)
+            )
+            if is_password_verified:
                 logger.warning("The user has logged in")
-                return("logged in")
+                return "logged in"
         logger.warning("The user failed to login")
-        return("login failed")
+        return "login failed"
