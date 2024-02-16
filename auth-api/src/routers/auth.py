d75f179d66ad2f6edc811fb1b565f47fe8c07efd
Edwin M <edwingmundo2@gmail.com>
2024-02-15 22:28:34 -0500

    Updates: adding valudate jwt code.

--- auth-api/src/routers/auth.py
+++ auth-api/src/routers/auth.py
@@ -1,13 +1,13 @@
 from fastapi import APIRouter, Depends, Response, HTTPException
-from fastapi.responses import JSONResponse
-from starlette.types import Message
 from loggingconf import setup_logging
 from sqlalchemy.orm import Session
-from utils.token import create_jwt_token
+from utils.token import create_jwt_token, verify_jwt_token
 from utils.hashing import verify_password
-from utils.db import get_db, User
+from utils.db import get_db
 from models.user_login import UserLogin
 from models.user import User
+from models.auth_token import AuthToken
+from jwt.exceptions import InvalidTokenError
 
 router = APIRouter(prefix="/api/v1/auth")
 logger = setup_logging()
@@ -46,4 +46,14 @@ async def login(
                 status_code=401, detail="Login failed invalid credentials"
             )
     logger.info(f"Login failed user {user_login.email} not found")
-    raise HTTPException(status_code=404, detail="Login Failed invalid credentials")
+    raise HTTPException(status_code=404, detail="Login failed invalid credentials")
+
+
+@router.post("/verify_token")
+def verify_auth_token(auth_token: AuthToken):
+    if auth_token.token:
+        try:
+            payload = verify_jwt_token(auth_token.token, auth_token.audience)
+            return payload
+        except InvalidTokenError:
+            raise HTTPException(status_code=401, detail="Login failed invalid token")
