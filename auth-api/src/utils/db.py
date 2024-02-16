1babcd5a180a37e63feba8a7eabd295fae8f481f
Edwin M <edwingmundo2@gmail.com>
2024-02-15 21:22:34 -0500

    Remove: Unused import
    Removed decalrative_base from imports this was moved into its own model

--- auth-api/src/utils/db.py
+++ auth-api/src/utils/db.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker
 from pathlib import Path
 from models.user import User
 from models.base import Base
