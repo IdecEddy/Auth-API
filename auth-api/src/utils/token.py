8dade5e74108066890667f986e078134d4d6f2c7
Edwin Mundo <edwingmundo2@gmail.com>
2025-04-19 08:25:55 -0400

    Change default expiration time for JWT token from 1 to 60 minutes

--- auth-api/src/utils/token.py
+++ auth-api/src/utils/token.py
@@ -26,7 +26,7 @@ else:
     raise IOError(f"Could not load public key from path {PUBLIC_KEY_PATH}")
 
 
-def create_jwt_token(user_id: int, audience: str, expires_delta: int = 1) -> str:
+def create_jwt_token(user_id: int, audience: str, expires_delta: int = 60) -> str:
     """
     Create a JWT token.
 
