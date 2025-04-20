ec933bd73d5d12d26f339045b92c43bb188be715
Edwin Mundo <edwingmundo2@gmail.com>
2025-04-19 20:58:43 -0400

    Update error messages for invalid token responses in verify_refresh_token

--- auth-api/src/routers/auth.py
+++ auth-api/src/routers/auth.py
@@ -56,9 +56,9 @@ def verify_refresh_token(refreshTokenRequest: RefreshTokenRequest):
             )
             return {"status": 200, "authToken": authToken}
         except InvalidTokenError as e:
-            raise HTTPException(status_code=401, detail="Login failed invalid token")
+            raise HTTPException(status_code=401, detail="Login failed invalid token 1")
     print(refreshTokenRequest.refreshToken)
-    return HTTPException(status_code=401, detail="Login failed invalid token")
+    return HTTPException(status_code=401, detail="Login failed invalid token 2")
 
 
 @router.post("/verify_auth_token")
