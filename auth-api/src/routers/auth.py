c74d73c3547a668bc72c57f4b0f5099998677281
Edwin Mundo <edwingmundo2@gmail.com>
2025-04-20 09:59:33 -0400

    Add logging for auth token in verify_tokens endpoint

--- auth-api/src/routers/auth.py
+++ auth-api/src/routers/auth.py
@@ -76,6 +76,8 @@ def verify_auth_token(authTokenRequest: AuthTokenRequest):
 
 @router.post("/verifyWithTokens")
 def verify_tokens(tokensAuthRequest: TokenAuthRequest):
+    if tokensAuthRequest.authToken:
+        logger.info("Trying to login with auth token")
     if tokensAuthRequest.refreshToken:
         logger.info("Trying to login with refresh token")
         logger.info(f"cookie vale: {tokensAuthRequest.refreshToken}")
