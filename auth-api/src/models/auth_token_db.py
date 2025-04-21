67ed43c64b8f364c5fed6df59c8052b38d9bb183
Edwin Mundo <edwingmundo2@gmail.com>
2025-04-21 00:47:48 -0400

    Add AuthTokenDB model for managing authentication tokens in the database

---
+++ auth-api/src/models/auth_token_db.py
@@ -0,0 +1,21 @@
+from sqlalchemy import DateTime, Column, Integer, String
+from sqlalchemy.sql import func
+from models.base import Base
+
+
+class AuthTokenDB(Base):
+    __tablename__ = "authAPI_Auth_Token"
+
+    id = Column(Integer, primary_key=True)
+    token = Column(String)
+    version = Column(Integer, unique=True)
+    date_created = Column(DateTime(timezone=True), server_default=func.now())
+    date_updated = Column(DateTime(timezone=True), onupdate=func.now())
+
+    def __repr__(self):
+
+        return (
+            f"<User(token={self.token}, version={self.version},"
+            f" date_created={self.date_created},"
+            f" date_updated={self.date_updated})>"
+        )
