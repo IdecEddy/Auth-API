f02cdbe22ed74191eb6923bec1956802f708e857
Edwin Mundo <edwingmundo2@gmail.com>
2025-04-25 00:17:17 -0400

    Fix create_jwt_token to correctly calculate expiration time using timedelta

--- auth-api/src/utils/token.py
+++ auth-api/src/utils/token.py
@@ -41,9 +41,7 @@ def create_jwt_token(
     Returns:
     str: Encoded JWT token.
     """
-    expire = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(
-        minutes=expires_delta
-    )
+    expire = datetime.datetime.now(datetime.timezone.utc) + expires_delta
     iss = "auth-api"
     sub = "device-id"
     aud = audience
